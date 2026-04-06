0e769065a7c751f925b0fa044d826e936f7b0275
Adri0205 <adrianaflores20501@gmail.com>
2026-04-06 17:08:49 -0600

    guia8

---
+++ guia8/NAVEGACION/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./src/screens/HomeScreen";
+import DetailsScreen from "./src/screens/DetailsScreen";
+
+const Drawer = createDrawerNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Details" component={DetailsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
